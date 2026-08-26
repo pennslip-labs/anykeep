ef3970c076210827db4756b978c8e0bd962379bc
Shamar Pennant <shamarpennant@gmail.com>
2026-08-26 10:13:15 -0400

    refreshed repo contents to reflect new developement plan

--- src/exporter.py
+++
@@ -1,8 +0,0 @@
-class Exporter:
-    """Placeholder exporter for future Anytype bundle generation."""
-
-    def __init__(self, output_path=None):
-        self.output_path = output_path
-
-    def export(self, objects):
-        return list(objects)
